3ea63a64861b92c5cb052c7afa699c0d3767ee98
MelissaMaximo <129124909+MelissaMaximo@users.noreply.github.com>
2025-03-27 16:23:21 -0300

    Correcao webhook Discord

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -9,18 +9,39 @@ const contactSchema = z.object({
 
 export async function POST(req: Request) {
   try {
+    // Lê o corpo da requisição
     const body = await req.json()
     const { name, email, message } = contactSchema.parse(body)
 
+    // Criação do payload para o Discord
+    const discordMessage = {
+      content: `Novo Contato:
+      Nome: ${name}
+      Email: ${email}
+      Mensagem: ${message}`
+    }
+
+    // Envia para o Discord
+    const webhookUrl =
+      'https://discord.com/api/webhooks/1354675483727630397/kHdfAXLsDpVsbCRp7VA84lngHpK8c48cgepYPml-zUwdO8GYrfBQms18nlUYPdgJcu1g'
+    const discordResponse = await fetch(webhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(discordMessage)
+    })
+
+    // Verifica se o envio foi bem-sucedido
+    if (!discordResponse.ok) {
+      throw new Error('Erro ao enviar a mensagem para o Discord')
+    }
+
+    // Retorna a resposta com sucesso
     return NextResponse.json({ success: true, data: { name, email, message } })
   } catch (error) {
-    // Tratamento seguro para o erro
-    const errorMessage =
-      error instanceof Error ? error.message : 'Erro desconhecido'
-
-    return NextResponse.json(
-      { success: false, error: errorMessage },
-      { status: 400 }
-    )
+    // Tratamento de erro
+    const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido'
+    return NextResponse.json({ success: false, error: errorMessage }, { status: 400 })
   }
 }
